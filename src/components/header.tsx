21e151b1958d65fac630116afeb3647883d87c05
unknown <rockph0029@gmail.com>
2023-10-16 15:46:09 +0800

    code changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,69 +1,91 @@
 "use client";
 
-import React , { useState}from "react";
-import { AiOutlineMenu as Menu, AiOutlineClose as Close} from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiOutlineMenu as Menu, AiOutlineClose as Close } from "react-icons/ai";
 
 type Props = {};
 
 export const Page = (props: Props) => {
-    const [nav,setNav] = useState<boolean>(false)
+  const [nav, setNav] = useState<boolean>(false);
   return (
     <>
       {" "}
-      <header className="flex justify-between items-center max-w-[1200px] mx-auto h-[84px] bg-[#F9FBFC] px-[24px]">
+      <header className="flex justify-between font-Poppins items-center max-w-[1200px] mx-auto h-[84px] bg-[#F9FBFC] px-[24px]">
         <h1 className="text-[22px] font-[600]">
           Course<span className="text-[#127C71]">space</span>
         </h1>
-        <ul className={`hidden md:flex gap-9 text-slate-500 `}>
+        <ul className={`hidden tracking-widest md:flex gap-9 cursor-pointer text-gray-400 leading-[4px] font-semibold `}>
           <li>
-            <a href="">Home</a>
+            <a className="hover:border-b-4 pb-1 rounded-bl-lg rounded-br-2xl hover:border-b-[#FFDD84]">Home</a>
           </li>
           <li>
-            <a href="">Courses</a>
+            <a className="hover:border-b-4 pb-1 rounded-bl-lg rounded-br-2xl hover:border-b-[#FFDD84]">Courses</a>
           </li>
           <li>
-            <a href="">Testimonial</a>
+            <a className="hover:border-b-4 pb-1 rounded-bl-lg rounded-br-2xl hover:border-b-[#FFDD84]">Testimonial</a>
           </li>
           <li>
-            <a href="">Mentor</a>
+            <a className="hover:border-b-4 pb-1 rounded-bl-lg rounded-br-2xl hover:border-b-[#FFDD84]">Mentor</a>
           </li>
         </ul>
-        <div className="hidden md:flex gap-3 ">
-          <button className="px-3 leading-5 py-2 hover:shadow-md border-2 border-[#127C71] rounded-lg text-[#127C71]">
+        <div className="hidden gap-3 md:flex ">
+          <button className="px-3 text-sm font-[500] py-2 hover:shadow-md border-2 border-[#127C71] rounded-lg text-[#127C71]">
             Sign In
           </button>
-          <button className="px-3 leading-5y-2 hover:shadow-md bg-[#127C71] rounded-lg text-white">
-            Sign Up p
+          <button className="px-3 text-sm font-[500] hover:shadow-md bg-[#127C71] rounded-lg text-white">
+            Sign Up
           </button>
         </div>
 
         {/* Hamburger */}
 
-        <div onClick={()=>setNav(prev=>!prev)} className="absolute right-4 block md:hidden">
+        <div
+          onClick={() => setNav((prev) => !prev)}
+          className="absolute block right-4 md:hidden"
+        >
           <Menu size={25} />
         </div>
 
         {/*  Mobile Menus */}
-        <div className={`absolute ${nav ? 'flex': 'hidden'} flex-row justify-center items-center left-0 top-0 h-screen w-full bg-white`}>
-          <Close onClick={()=>setNav(false)} size={25} className="absolute right-5 top-5" />
-          <ul className="gap-5 flex flex-col text-center -mt-6 text-slate-500 text-lg">
-            <li>
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="">Courses</a>
-            </li>
-            <li>
-              <a href="">REsources</a>
-            </li>
-            <li>
-              <a href="">Mentor</a>
-            </li>
-          </ul>
-          <div className="absolute bottom-6 h-16 w-full flex justify-center items-center gap-2">
+        <div
+          className={`absolute ${
+            nav ? "h-screen" : "h-0"
+          } duration-500 flex-row justify-center items-center left-0 top-0 h-screen w-full bg-[#F9FBFC]`}
+        >
+          <Close
+            onClick={() => setNav(false)}
+            size={25}
+            className={`absolute right-5 top-5 ${!nav && "hidden"}`}
+          />
+          <div className="absolute w-full bottom-[55%]">
+            <ul className="flex flex-col gap-5 -mt-6 text-lg text-center text-slate-500">
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Courses</a>
+              </li>
+              <li>
+                <a href="">REsources</a>
+              </li>
+              <li>
+                <a href="">Mentor</a>
+              </li>
+            </ul>
+          </div>
+          <div className="absolute flex items-center justify-center w-full h-16 gap-2 bottom-6">
             <button className="px-3 py-2 border-2 border-[#127C71] rounded-lg text-[#127C71]">Sign In</button>
             <button className="px-3 py-2 bg-[#127C71] rounded-lg text-white">Sign Up</button>
           </div>
+
+          {/* <div className="absolute flex items-center justify-center w-full h-16 gap-2 bottom-6">
+              <button className="px-3 py-2 border-2 border-[#127C71] rounded-lg text-[#127C71]">
+                Sign In
+              </button>
+              <button className="px-3 py-2 bg-[#127C71] rounded-lg text-white">
+                Sign Up
+              </button>
+            </div> */}
         </div>
       </header>
     </>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,13 +4,35 @@ type Props = {}
 
 export const Page = (props: Props) => {
   return (
-    <section className='bg-[#F9FBFC] max-w-[1200px] mx-auto'>
-        <div>
-            <div className='flex'>
-                <div className='basis-[60%] bg-blue-400'>rex</div>
+    <section className="bg-[#F9FBFC] max-w-[1200px] mx-auto py-10">
+      <div>
+        <div className="flex flex-col px-3 md:flex-row">
+          {/* <div className="">
+            <h1 className="mx-auto md:mx-0 text-4xl md:text-6xl md:max-w-[500px] max-w-[350px] font-bold text-center font-Poppins">
+              <span className="text-[#127C71]">Improve</span> your skill with
+              different way
+            </h1>  
+            <p>Lets take</p>    
+          </div> */}
+          <div className=" md:basis-2/3">
+            <h1 className="mx-auto text-3xl w-[300px] md:w-full font-bold leading-tight md:mx-0 md:text-6xl font-Poppins">
+              <span className="text-[#127C71] border-b-4 border-[#FFDD84] leading-loose">Improve</span> your skill with
+              different way.
+            </h1>
+            <p className="mt-6 leading-loose text-base text-center md:w-[450px] mx-3 md:mx-0 text-gray-500 md:text-start">
+              Lets take an online course to improve your skills in a different
+              way, you can set your own study time according to your learning
+              speed. So you can study comfortable and asborb the material
+              easily.
+            </p>
+            <div className='flex justify-center gap-3 mt-6 md:justify-start'>
+                <button className='px-3 py-2 border text-[#127C71] font-semibold border-[#127C71] rounded-lg'>Get Started</button>
+                <button className='px-3 py-2 bg-[#127C71] rounded-lg font-semibold text-white'>Watch Video</button>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
